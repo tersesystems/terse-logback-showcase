fc93796ba9b3f71aba7c1f718649218f2252e3de
Will Sargent <will@tersesystems.com>
2020-03-08 12:17:39 -0700

    Add honeycomb links

--- app/controllers/LogController.java
+++ app/controllers/LogController.java
@@ -1,18 +1,21 @@
 package controllers;
 
+import ch.qos.logback.classic.Logger;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.typesafe.config.Config;
 import logging.LogEntry;
 import logging.LogEntryFinder;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import play.libs.Json;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
-import play.mvc.Results;
 import scala.collection.JavaConverters;
 
 import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -21,17 +24,41 @@ import java.util.concurrent.CompletionStage;
 public class LogController extends Controller {
 
     private final LogEntryFinder finder;
+    private final String dataSet;
+    private final String team;
+    private final boolean honeycombEnabled;
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 
     @Inject
-    public LogController(LogEntryFinder finder) {
+    public LogController(Config config, LogEntryFinder finder) {
         this.finder = finder;
+        this.dataSet = config.getString("honeycomb.dataSet");
+        this.team = config.getString("honeycomb.team");
+        this.honeycombEnabled = config.getBoolean("honeycomb.enabled");
     }
 
     public CompletionStage<Result> show(String id) {
         return finder.findById(id).thenApply(maybeEntry ->
-                maybeEntry.map(entry -> ok(views.html.log.show.render(entry)))
-                          .orElse(notFound()));
-    } 
+                maybeEntry.map(entry -> {
+                    String honeycombLink = null;
+                    if (isHoneycombEnabled()) {
+                        JsonNode json = Json.parse(entry.event());
+                        String traceId = json.findPath("trace.trace_id").asText();
+                        logger.info("traceId = {}", traceId);
+                        if (!traceId.equals("")) {
+                            long startTSE = entry.timestamp().toEpochMilli();
+                            long endTSE = entry.timestamp().plusSeconds(60).toEpochMilli();
+                            honeycombLink = views.html.log.honeycomb.render(team, dataSet, traceId, startTSE, endTSE).body();
+                        }
+                    }
+                    logger.info("honeycombLink {}", honeycombLink);
+                    return ok(views.html.log.show.render(entry, scala.Option.apply(honeycombLink)));
+                }).orElse(notFound()));
+    }
+
+    private boolean isHoneycombEnabled() {
+        return this.honeycombEnabled;
+    }
 
     public CompletionStage<Result> list(Integer page) {
         int mult = Math.max(page, 1) - 1;
